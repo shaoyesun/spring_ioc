bd6387cfafc138adace639634c811afbc28b4e5b
sunjf <sunjf@biomarker.com.cn>
2017-08-03 11:50:37 +0800

    life

--- src/main/java/com/ioc/cycle/MyBeanPostProcessor.java
+++ src/main/java/com/ioc/cycle/MyBeanPostProcessor.java
@@ -4,16 +4,16 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 /**
- * 后置处理器
+ * bean后处理器
  */
 public class MyBeanPostProcessor implements BeanPostProcessor {
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
-        System.out.println("前置处理器：MyBeanPostProcessor postProcessBeforeInitialization " + o + " " + s);
+        System.out.println("bean后处理器,前置处理：MyBeanPostProcessor postProcessBeforeInitialization " + o + " " + s);
         return o;
     }
 
     public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
-        System.out.println("后置处理器：MyBeanPostProcessor postProcessAfterInitialization " + o + " " + s);
+        System.out.println("bean后处理器,后置处理：MyBeanPostProcessor postProcessAfterInitialization " + o + " " + s);
         return o;
     }
 }
