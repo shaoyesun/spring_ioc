d993716410ca361dfb9b216c3e304bff4983dbc5
sunjf <sunjf@biomarker.com.cn>
2017-08-06 09:53:16 +0800

    factoryBean

---
+++ src/main/java/com/ioc/factorybean/Model.java
@@ -0,0 +1,15 @@
+package com.ioc.factorybean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Administrator on 2017/8/2.
+ */
+public class Model {
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("factorybean.xml");
+        User user = (User) ac.getBean("myFactoryBean");
+        System.out.println(user.toString());
+    }
+}
---
+++ src/main/java/com/ioc/factorybean/MyFactoryBean.java
@@ -0,0 +1,36 @@
+package com.ioc.factorybean;
+
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * Created by Administrator on 2017/8/2.
+ */
+public class MyFactoryBean implements FactoryBean<User>{
+
+    private User user;
+
+    public User getObject() throws Exception {
+        System.out.println("begin user bean");
+        User user = new User();
+        user.setName("factoryBean");
+        user.setAge(12);
+        System.out.println("end user bean");
+        return user;
+    }
+
+    public Class<?> getObjectType() {
+        return User.class;
+    }
+
+    public boolean isSingleton() {
+        return false;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
